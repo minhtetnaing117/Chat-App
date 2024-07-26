88bbd3770ee9c2b2ba86424b48301bdc4eaf2655
minhtetnaing117 <59909903+minhtetnaing117@users.noreply.github.com>
2024-07-26 20:31:49 +0630

    initial text

---
+++ lib/components/my_textfield.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class MyTextfield extends StatelessWidget {
+  const MyTextfield({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      decoration: InputDecoration(
+        enabledBorder: OutlineInputBorder(
+          borderSide: BorderSide(color: Theme.of(context).colorScheme.tertiary),
+        ),
+      ),
+
+    );
+  }
+}
