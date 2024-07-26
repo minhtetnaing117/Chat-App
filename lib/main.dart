31dcbb4148bb1f3239587ee7be8b5cf2d52dfe53
minhtetnaing117 <59909903+minhtetnaing117@users.noreply.github.com>
2024-07-26 20:26:52 +0630

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: LoginPage(),
+      home: const LoginPage(),
       theme: lightMode,
     );
   }
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -7,6 +7,35 @@ class LoginPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.background,
+      body: Center(
+        child:Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // logo
+            Icon(
+              Icons.message,
+              size: 60,
+              color: Theme.of(context).colorScheme.primary,
+              ),
+
+            const SizedBox(height: 50),
+
+            // welcome back message
+            Text(
+              "Welcome back, you've been missed!",
+              style: TextStyle(
+                color: Theme.of(context).colorScheme.primary,
+                fontSize: 16,
+              ),
+            ),
+
+            const SizedBox(height: 25),
+
+            // email textfield
+            TextField(),
+          ],
+        ),
+      ),
     );
   }
 }
