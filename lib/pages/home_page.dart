41f6c24d6478857fbc58f07026425f73353f09d5
minhtetnaing117 <59909903+minhtetnaing117@users.noreply.github.com>
2024-09-29 15:10:42 +0630

    add user_tile, chat page, _buildUserList and _buildUserListItem

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,8 +1,22 @@
 import 'package:chat_app/components/my_drawer.dart';
+import 'package:chat_app/components/user_tile.dart';
+import 'package:chat_app/pages/chat_page.dart';
+import 'package:chat_app/services/auth/auth_service.dart';
+import 'package:chat_app/services/chat/chat_service.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+  HomePage({super.key});
+
+  // chat & auth service
+  final ChatService _chatServive = ChatService();
+  final AuthService _authService = AuthService();
+
+  void logout(){
+    // get auth service
+    final auth = AuthService();
+    auth.signOut();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,6 +25,50 @@ class HomePage extends StatelessWidget {
         title: Text("Home"),
       ),
       drawer: MyDrawer(),
+      body: _buildUserList()
+    );
+  }
+
+  // build a list of users except for the current logged in user
+  Widget _buildUserList(){
+    return StreamBuilder(
+      stream: _chatServive.getUsersStream(),
+      builder: (context, snapshot){
+        // error
+        if (snapshot.hasError) {
+          return const Text("error");
+        }
+
+        // loading
+        if(snapshot.connectionState == ConnectionState.waiting){
+          return const Text('Loading...');
+        }
+
+        // return list view
+        return ListView(
+          children: snapshot.data!.
+          map<Widget>((userData) => _buildUserListItem(userData, context))
+          .toList(),
+        );
+      }
+    );
+  }
+
+  // build invidual list title for user
+  Widget _buildUserListItem(Map<String, dynamic> userData, BuildContext context){
+    return UserTile(
+      text: userData["email"],
+      onTap: (){
+        // tapped on a user -> go to chat page
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => ChatPage(
+              receiverEmail: userData['email'],
+            ),
+            )
+        );
+      }
     );
   }
 }
